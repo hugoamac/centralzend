81b1b111e0501cbc0d109c5db733cf1f7d4e2772
hugo machado <hugoamac@live.com>
2013-01-24 15:21:04 -0300

    phpunit configurado

---
+++ tests/application/models/UsuarioTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class UsuarioTest extends PHPUnit_Framework_TestCase{
+    
+    
+    public function testInsereUsuario()
+    {
+        $data =1;
+        $this->assertEquals($data,1);
+    }
+    
+}
+
