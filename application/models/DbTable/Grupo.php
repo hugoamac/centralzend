d0f7221c706aa9002b2727a7ab619994452b037f
hugo machado <hugoamac@live.com>
2013-01-25 22:02:49 -0300

    minha conta

--- application/models/DbTable/Grupo.php
+++ application/models/DbTable/Grupo.php
@@ -4,6 +4,11 @@ class Application_Model_DbTable_Grupo extends Zend_Db_Table_Abstract {
 
     protected $_name = 'usu_grupo';
     
+    public function getSql(){
+        
+        return "SELECT * FROM usu_grupo";
+    }
+    
 
 }
 
